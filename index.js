7b04cb4562d86c5d276f1f5c08d2a4e215b11485
Campbell <122378640+campbell-r-e@users.noreply.github.com>
2025-08-28 15:51:16 +0000

    intial

---
+++ index.js
@@ -0,0 +1,14 @@
+
+
+function fibonacci(n){
+    if(n==0 || n==1)
+        return n;
+    return fibonacci(n-1) + fibonacci(n-2);
+}
+main()
+function main(){
+    console.log("we")
+    for(x=0; x<3; x++){
+    const w = fibonacci(x);
+    console.log(w);}
+}
